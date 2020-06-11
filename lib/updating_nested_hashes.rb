0bcb47e8652bd12d6cefb01fcd85474521a7560a
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-11 23:19:49 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,8 +63,8 @@ def add_characters
 epic_tragedy[:additional_characters] = []
 prince = {name: "Prince Escalus"}
 apothecary = { name: "Apothecary"}
-epic_tragedy[:additional_characters] << {name: "Prince Escalus"}
-epic_tragedy[:additional_characters] << {name: "Apothecary"}
+epic_tragedy[:additional_characters] << prince
+epic_tragedy[:additional_characters] << apothecary
 
 
 
